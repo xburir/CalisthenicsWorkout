9c4b487a638853d0045f5741f9e5a4aa4b91643e
Richard <xburir@stuba.sk>
2021-12-07 01:43:19 +0100

    dark mode disabled

--- app/src/main/java/com/example/calisthenicsworkout/MainActivity.kt
+++ app/src/main/java/com/example/calisthenicsworkout/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.calisthenicsworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
@@ -22,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         drawerLayout = binding.drawerLayout
 
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
 
 
