58eb53b5446294160810bbc65d77b9d5ad01c169
Richard <xburir@stuba.sk>
2021-12-26 15:06:11 +0100

    video download progress showed

--- app/src/main/java/com/example/calisthenicsworkout/VideoActivity.kt
+++ app/src/main/java/com/example/calisthenicsworkout/VideoActivity.kt
@@ -2,35 +2,52 @@ package com.example.calisthenicsworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.MediaController
 import android.widget.Toast
 import android.widget.VideoView
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.viewModelScope
+import com.example.calisthenicsworkout.databinding.ActivityVideoBinding
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.coroutines.launch
+import java.io.File
 
 class VideoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityVideoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_video)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_video)
+
+
         val fbStorage = FirebaseStorage.getInstance()
         val skillId = intent.getStringExtra("skillId")
         val videoHolder = findViewById<VideoView>(R.id.videoView)
-        val videoRef = fbStorage.reference.child("skillVideos").child("$skillId.mp4")
-        videoRef.downloadUrl
-            .addOnSuccessListener {
-                Toast.makeText(this,"Video is downloading...",Toast.LENGTH_SHORT).show()
-                videoHolder.setVideoURI(it)
-                val mediaController = MediaController(this)
-                mediaController.setAnchorView(videoHolder)
-                videoHolder.setMediaController(mediaController)
-                videoHolder.start()
-            }
+        val localFile = File.createTempFile("video", "mp4")
+
+        fbStorage.reference.child("skillVideos").child("$skillId.mp4").getFile(localFile)
             .addOnFailureListener {
-                Toast.makeText(this,"Video not found",Toast.LENGTH_SHORT).show()
+                binding.progressText.text = "Video not found"
+                binding.progressBar2.visibility = View.GONE
             }
+            .addOnCompleteListener {
+                if(it.isSuccessful){
+                    binding.progressText.visibility = View.GONE
+                    binding.progressBar2.visibility = View.GONE
+                    binding.videoView.visibility = View.VISIBLE
 
-
+                    videoHolder.setVideoPath(localFile.absolutePath)
+                    val mediaController = MediaController(this)
+                    mediaController.setAnchorView(videoHolder)
+                    videoHolder.setMediaController(mediaController)
+                    videoHolder.start()
+                }
+            }
+            .addOnProgressListener {
+                val progress = (100*it.bytesTransferred/it.totalByteCount)
+                binding.progressBar2.progress = progress.toInt()
+                binding.progressText.text = "$progress%"
+            }
 
     }
 }
